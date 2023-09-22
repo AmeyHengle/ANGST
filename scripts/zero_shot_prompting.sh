4238b164d82b9ba986de580fd0dbafbbd31d81cc
mr-atharva-kulkarni <atharva.j.kulkarni1998@gmail.com>
2023-09-22 16:57:59 -0400

    adding depression mards gpt4 results

--- scripts/zero_shot_prompting.sh
+++ scripts/zero_shot_prompting.sh
@@ -3,8 +3,8 @@
 SEED=0
 DATA_PATH="./data/test/full_test.csv"
 RESULT_DIR="./results/zero_shot"
-MODEL="gpt-3.5-turbo"
-# MODEL="gpt-4"
+# MODEL="gpt-3.5-turbo"
+MODEL="gpt-4"
 
 # choices=[
 #     'depression_mards', 
