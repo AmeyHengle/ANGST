18992dc25f244f527343f751c2baa5e04a04b401
AmeyHengle <ameyhengle22@gmail.com>
2023-10-09 04:03:31 +0530

    add multilabel-clf scripts for hyp tuning

---
+++ multilabel-clf/trainer3.sh
@@ -0,0 +1,276 @@
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 4 \
+    --learning_rate 2e-5 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 1 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 8 \
+    --learning_rate 2e-5 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 1 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 16 \
+    --learning_rate 2e-5 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 1 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 32 \
+    --learning_rate 2e-5 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 1 \
+;
+
+# ----------------------------------------------------------------------------------- #
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 4 \
+    --learning_rate 2e-4 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 1 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 8 \
+    --learning_rate 2e-4 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 1 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 16 \
+    --learning_rate 2e-4 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 1 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 32 \
+    --learning_rate 2e-4 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 1 \
+;
+
+# ----------------------------------------------------------------------------------- #
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 4 \
+    --learning_rate 2e-5 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 2 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 8 \
+    --learning_rate 2e-5 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 2 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 16 \
+    --learning_rate 2e-5 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 2 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 32 \
+    --learning_rate 2e-5 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 2 \
+;
+
+# ----------------------------------------------------------------------------------- #
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 4 \
+    --learning_rate 2e-4 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 2 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 8 \
+    --learning_rate 2e-4 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 2 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 16 \
+    --learning_rate 2e-4 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 2 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 32 \
+    --learning_rate 2e-4 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 2 \
+;
+
+# ----------------------------------------------------------------------------------- #
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 4 \
+    --learning_rate 2e-5 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 3 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 8 \
+    --learning_rate 2e-5 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 3 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 16 \
+    --learning_rate 2e-5 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 3 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 32 \
+    --learning_rate 2e-5 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 3 \
+;
+
+# ----------------------------------------------------------------------------------- #
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 4 \
+    --learning_rate 2e-4 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 3 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 8 \
+    --learning_rate 2e-4 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 3 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 16 \
+    --learning_rate 2e-4 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 3 \
+;
+
+python trainer.py \
+    --model_name AIMH/mental-roberta-large \
+    --text_col text \
+    --max_length 512 \
+    --batch_size 32 \
+    --learning_rate 2e-4 \
+    --num_epochs 30 \
+    --metric_name f1 \
+    --random_state 3 \
+;
+
+# ----------------------------------------------------------------------------------- #
+
