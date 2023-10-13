3b246c5fd0793bc1cde38951e11178c9d36929d3
mr-atharva-kulkarni <atharva.j.kulkarni1998@gmail.com>
2023-10-13 10:21:36 -0400

    adding flan_t5 zero_shot anxiety and comorbidity v1, v2, v3 results

--- scripts/prompting_zero_shot_llm.sh
+++ scripts/prompting_zero_shot_llm.sh
@@ -6,21 +6,21 @@
 #SBATCH --gres gpu:4
 #SBATCH --mem-per-gpu=40GB
 #SBATCH --time=2-23:00:00
-#SBATCH --output=logs/zero_shot_llm/zero_shot_depression_mental_flan_t5_v1.log
-#SBATCH --error=errors/zero_shot_llm/zero_shot_depression_mental_flan_t5_v1.err
+#SBATCH --output=logs/zero_shot_llm/zero_shot_comorbidity_mental_flan_t5_v3.log
+#SBATCH --error=errors/zero_shot_llm/zero_shot_comorbidity_mental_flan_t5_v3.err
 
 source activate llm_env
 
 DATA_PATH="./data/test/full_test.csv"
 RESULT_DIR="./results/zero_shot"
-VERSION=1
+VERSION=3
 
 MODEL="mental_flan_t5"
 # MODEL="mental_alpaca"
 
-PROMPT_TYPE="depression"
+# PROMPT_TYPE="depression"
 # PROMPT_TYPE="anxiety"
-# PROMPT_TYPE="comorbidity"
+PROMPT_TYPE="comorbidity"
 
 python3 src/prompting_zero_shot.py \
 --seed $VERSION \
