3c7089e8d00fd22f681b0196e8986e85204f9310
AmeyHengle <ameyhengle22@gmail.com>
2023-10-20 10:38:50 +0530

    add chmod command

--- flant5/scripts/train_and_predict_mental-flant5-xxl.sh
+++ flant5/scripts/train_and_predict_mental-flant5-xxl.sh
@@ -3,11 +3,34 @@
 # chmod +x scripts/model/train_and_predict.sh
 # ./scripts/model/train_and_predict.sh model_name
 
-bash scripts/train_model.sh train_"train_xxl_mh_depression"
-bash scripts/predict_model.sh "test_xxl_mh_depression"
+python src/train.py --gin_file="scripts/model.gin" --gin_file="exp/configs/train_model_mental_flant5_xxl_3_epoch.gin" --gin.MODEL_DIR="'checkpoints/mental-flant5-xxl-depression'" --gin.MODE="'train'" --gin.SPLIT="'depression'"
+python src/train.py --gin_file="scripts/model.gin" --gin_file="exp/configs/train_model_mental_flant5_xxl_3_epoch.gin" --gin.MODEL_DIR="'checkpoints/mental-flant5-xxl-anxiety'" --gin.MODE="'train'" --gin.SPLIT="'anxiety'"
+python src/train.py --gin_file="scripts/model.gin" --gin_file="exp/configs/train_model_mental_flant5_xxl_3_epoch.gin" --gin.MODEL_DIR="'checkpoints/mental-flant5-xxl-comorbidity'" --gin.MODE="'train'" --gin.SPLIT="'comorbidity'"
 
-bash scripts/train_model.sh train_"train_xxl_mh_anxiety"
-bash scripts/predict_model.sh "test_xxl_mh_anxiety"
 
-bash scripts/train_model.sh train_"train_xxl_mh_comorbidity"
-bash scripts/predict_model.sh "test_xxl_mh_comorbidity"
+python src/inference.py \
+    --gin_file="scripts/model_inference.gin" \
+    --gin.MODEL_DIR="'checkpoints/mental-flant5-xxl-depression'" \
+    --gin.EVALUATE_METRICS="['bleu', 'bertscore', 'rouge']" \
+    --gin.RESULT_FILE="'checkpoints/mental-flant5-xxl-depression/results.csv'" \
+    --gin.MODE="'test'" \
+    --gin.SPLIT="'depression'" \
+    --gin.BATCH_SIZE=16
+
+python src/inference.py \
+    --gin_file="scripts/model_inference.gin" \
+    --gin.MODEL_DIR="'checkpoints/mental-flant5-xxl-anxiety'" \
+    --gin.EVALUATE_METRICS="['bleu', 'bertscore', 'rouge']" \
+    --gin.RESULT_FILE="'checkpoints/mental-flant5-xxl-anxiety/results.csv'" \
+    --gin.MODE="'test'" \
+    --gin.SPLIT="'anxiety'" \
+    --gin.BATCH_SIZE=16
+
+python src/inference.py \
+    --gin_file="scripts/model_inference.gin" \
+    --gin.MODEL_DIR="'checkpoints/mental-flant5-xxl-comorbidity'" \
+    --gin.EVALUATE_METRICS="['bleu', 'bertscore', 'rouge']" \
+    --gin.RESULT_FILE="'checkpoints/mental-flant5-xxl-comorbidity/results.csv'" \
+    --gin.MODE="'test'" \
+    --gin.SPLIT="'comorbidity'" \
+    --gin.BATCH_SIZE=16
